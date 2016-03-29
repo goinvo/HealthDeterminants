f84134d52675fad4bd0329850f3df0440d895f29
YvonneChow <naruto3135@gmail.com>
2016-03-29 14:31:27 -0400

    add mobile version for graph

--- js/min/mobile_metrix-min.js
+++ js/min/mobile_metrix-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$.getJSON("health-determinants.json",function(n){console.log(n)})});
+$(document).ready(function(){$(".carousel").carousel(),$.getJSON("health-determinants.json",function(e){console.log(e);var t=$("#metrixTemplate").html(),l=Mustache.to_html(t,e);$("#mobile-metrix .carousel-inner").html(l),$("#mobile-metrix .item:first-child").addClass("active")})});
--- js/mobile_metrix.js
+++ js/mobile_metrix.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
+
+	$('.carousel').carousel();
+
 	$.getJSON('health-determinants.json', function(data) {
 	    console.log(data)
+	    var metrixTemplate = $('#metrixTemplate').html();
+	    var html = Mustache.to_html(metrixTemplate, data);
+	      $('#mobile-metrix .carousel-inner').html(html);
+
+
+	    $("#mobile-metrix .item:first-child").addClass('active');
 	});
 });
