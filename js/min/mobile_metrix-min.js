06a912228b68c51f71a6eb379c1df39bb1f85b2e
YvonneChow <naruto3135@gmail.com>
2016-03-31 10:35:07 -0400

    start adding core health matrix

--- js/min/mobile_metrix-min.js
+++ js/min/mobile_metrix-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".carousel").carousel(),$.getJSON("health-determinants.json",function(e){console.log(e);var t=$("#metrixTemplate").html(),l=Mustache.to_html(t,e);$("#mobile-metrix .carousel-inner").html(l),$("#mobile-metrix .item:first-child").addClass("active")})});
+$(document).ready(function(){$(".carousel").carousel(),$.getJSON("health-determinants.json",function(e){console.log(e);var t=$("#matrixTemplate").html(),a=Mustache.render(t,e);$("#mobile-matrix .carousel-inner").html(a),$("#mobile-matrix .item:first-child").addClass("active")})});
--- js/mobile_metrix.js
+++ js/mobile_metrix.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
 
 	$.getJSON('health-determinants.json', function(data) {
 	    console.log(data)
-	    var metrixTemplate = $('#metrixTemplate').html();
-	    var html = Mustache.to_html(metrixTemplate, data);
-	      $('#mobile-metrix .carousel-inner').html(html);
+	    var matrixTemplate = $('#matrixTemplate').html();
+	    var html = Mustache.render(matrixTemplate, data);
+	      $('#mobile-matrix .carousel-inner').html(html);
 
 
-	    $("#mobile-metrix .item:first-child").addClass('active');
+	    $("#mobile-matrix .item:first-child").addClass('active');
 	});
 });
