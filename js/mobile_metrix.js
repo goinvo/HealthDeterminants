9ceb99b4705c094b4a41c0be3328bd8b4b192de6
YvonneChow <naruto3135@gmail.com>
2016-04-01 13:42:36 -0400

    add view more for core table

--- js/mobile_metrix.js
+++ js/mobile_metrix.js
@@ -13,5 +13,23 @@ $(document).ready(function() {
 	});
 
 	// core health table
-	$("#core-health-table tr:nth-child(n+21)").hide();
+	$("#core-health-matrix table tr:nth-child(n+21)").hide();
+	var restRows = $("#core-health-matrix table tbody tr").length - 20;
+	$("#rest-row-number").text(restRows);
+
+	$("#view-more-link").click(function(event) {
+		/* Act on the event */
+		$("#core-health-matrix table tr:nth-child(n+21)").toggle("slow", function(){
+			$("#view-more-link .glyphicon").toggleClass("glyphicon-menu-down glyphicon-menu-up");
+
+			if($("#view-more-link .glyphicon").hasClass('glyphicon-menu-up')) {
+				$("#view-more-content").html('view the first <span id="rest-row-number">20</span> rows </span>');
+			}
+			else {
+				$("#view-more-content").html('view the rest <span id="rest-row-number"></span> rows </span>');
+				$("#rest-row-number").text(restRows);
+			}
+		});
+		
+	});
 });
