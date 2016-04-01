a9db6dc399f2d6395b192bc6bfe268b5e668eb9c
YvonneChow <naruto3135@gmail.com>
2016-04-01 13:57:16 -0400

    update view more text

--- js/mobile_metrix.js
+++ js/mobile_metrix.js
@@ -14,8 +14,8 @@ $(document).ready(function() {
 
 	// core health table
 	$("#core-health-matrix table tr:nth-child(n+21)").hide();
-	var restRows = $("#core-health-matrix table tbody tr").length - 20;
-	$("#rest-row-number").text(restRows);
+	// var restRows = $("#core-health-matrix table tbody tr").length - 20;
+	// $("#rest-row-number").text(restRows);
 
 	$("#view-more-link").click(function(event) {
 		/* Act on the event */
@@ -23,11 +23,10 @@ $(document).ready(function() {
 			$("#view-more-link .glyphicon").toggleClass("glyphicon-menu-down glyphicon-menu-up");
 
 			if($("#view-more-link .glyphicon").hasClass('glyphicon-menu-up')) {
-				$("#view-more-content").html('view the first <span id="rest-row-number">20</span> rows </span>');
+				$("#view-more-content").html('view top <span id="rest-row-number">20</span> rows </span>');
 			}
 			else {
-				$("#view-more-content").html('view the rest <span id="rest-row-number"></span> rows </span>');
-				$("#rest-row-number").text(restRows);
+				$("#view-more-content").html('view all metrics</span>');
 			}
 		});
 		
