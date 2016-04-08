12bb1155cf593a83a2cde975d369e15a8558837d
YvonneChow <naruto3135@gmail.com>
2016-04-08 13:14:27 -0400

    add alternative treemap

--- js/mobile_metrix.js
+++ js/mobile_metrix.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	$('.carousel').carousel();
 
-	$.getJSON('health-determinants.json', function(data) {
+	$.getJSON('data/health-determinants.json', function(data) {
 	    //console.log(data)
 	    var matrixTemplate = $('#matrixTemplate').html();
 	    var html = Mustache.render(matrixTemplate, data);
